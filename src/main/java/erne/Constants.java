db8ad01b52b94309166be81b978a28282d6f0864
unknown <huystalker@gmail.com>
2015-07-03 01:19:23 +0900

    Big run

--- src/main/java/erne/Constants.java
+++ src/main/java/erne/Constants.java
@@ -3,7 +3,7 @@ package erne;
 public class Constants {
 
 	public static final boolean autoSpeciationThreshold = true;
-	public static final int targetNSpecies = 10;
+	public static final int targetNSpecies = 20;
 	public static final double speciationThresholdMod = 0.1;
 	public static final double speciationThresholdMin = 0.03;
 	public static final double probMutationOnly = 0.5;
--- src/main/java/use/math/Run.java
+++ src/main/java/use/math/Run.java
@@ -49,11 +49,11 @@ public class Run {
 
 		ReflectionUI reflectionUI = new ReflectionUI();
 
-		Population population = new Population(200, Library.startingMath);
+		Population population = new Population(500, Library.startingMath);
 		population.setFitnessFunction(new GaussianFitnessFunction());
 		population.setMutator(new Mutator(new ArrayList<MutationRule>(Arrays.asList(new MutationRule[] { new DisableTemplate(1),
 				new MutateParameter(90), new AddNode(2), new AddActivation(2), new AddInhibition(5) }))));
-		for (int i = 0; i < 100; i++) {
+		for (int i = 0; i < 10000; i++) {
 			if (i == 0) {
 				population.resetPopulation();
 			} else {
