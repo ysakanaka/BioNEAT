88c454e69a2af0f1eaa715013f22101052a22160
unknown <huystalker@gmail.com>
2015-07-02 18:56:30 +0900

    Better Gaussian fitter

--- src/main/java/use/math/GaussianFitnessFunction.java
+++ src/main/java/use/math/GaussianFitnessFunction.java
@@ -23,40 +23,47 @@ public class GaussianFitnessFunction extends AbstractMathFitnessFunction {
 		final WeightedObservedPoints obs = new WeightedObservedPoints();
 		for (int i = 0; i < tests.size(); i++) {
 			obs.add(tests.get(i)[0], actualOutputs[i]);
-		}
+			System.out.println(tests.get(i)[0] + " " + actualOutputs[i]);
 
-		final GaussianCurveFitter fitter = GaussianCurveFitter.create().withMaxIterations(1000).withStartPoint(targetCoeff);
+		}
+		double[] coeff = new double[] { 0, 25, 3.5 };
+		final GaussianCurveFitter fitter = GaussianCurveFitter.create().withMaxIterations(1000);
 		try {
-			double[] coeff = fitter.fit(obs.toList());
-			FitnessResult result = new FitnessResult(false);
-
-			result.tests = tests;
-			result.actualOutputs = actualOutputs;
-
-			double[] targetOutputs = new double[tests.size()];
-			for (int i = 0; i < tests.size(); i++) {
-				obs.add(tests.get(i)[0], actualOutputs[i]);
-				targetOutputs[i] = coeff[0] * Math.exp((-Math.pow(tests.get(i)[0] - coeff[1], 2)) / (2 * Math.pow(coeff[2], 2)));
-			}
-			result.targetOutputs = targetOutputs;
-
-			result.targetFittingParams = targetCoeff;
-			result.actualFittingParams = new double[] { coeff[0], coeff[1], coeff[2] };
-			return result;
+			coeff = fitter.fit(obs.toList());
 		} catch (TooManyIterationsException e) {
-			return (FitnessResult) minFitness();
+
 		}
+		FitnessResult result = new FitnessResult(false);
+
+		result.tests = tests;
+		result.actualOutputs = actualOutputs;
+
+		double[] targetOutputs = new double[tests.size()];
+		for (int i = 0; i < tests.size(); i++) {
+			targetOutputs[i] = coeff[0] * Math.exp((-Math.pow(tests.get(i)[0] - coeff[1], 2)) / (2 * Math.pow(coeff[2], 2)));
+		}
+		result.targetOutputs = targetOutputs;
+
+		result.targetFittingParams = targetCoeff;
+		result.actualFittingParams = new double[] { coeff[0], coeff[1], coeff[2] };
+		return result;
 
 	}
 
 	public static void main(String[] args) {
 		final WeightedObservedPoints obs = new WeightedObservedPoints();
-		Random rand = new Random();
-		for (int i = 0; i < 100; i++) {
-			obs.add(i, 0.0000000000001 * Math.exp((-Math.pow(i - 25, 2)) / (2 * Math.pow(3.5, 2))));
-		}
+		obs.add(1.0, 0.041084924203786885);
+		obs.add(6.444444444444445, 0.00822427625858405);
+		obs.add(11.88888888888889, 0.004128547441396287);
+		obs.add(17.333333333333336, 0.0026128182678188086);
+		obs.add(22.77777777777778, 0.001850039379161948);
+		obs.add(28.22222222222222, 0.0014016113563683381);
+		obs.add(33.66666666666667, 0.0011114454869482894);
+		obs.add(39.111111111111114, 9.109175255016486E-4);
+		obs.add(44.55555555555556, 7.654880728002641E-4);
+		obs.add(50.0, 6.560391667093474E-4);
 
-		final GaussianCurveFitter fitter = GaussianCurveFitter.create().withMaxIterations(1000);
+		final GaussianCurveFitter fitter = GaussianCurveFitter.create();
 
 		final double[] coeff = fitter.fit(obs.toList());
 		System.out.println(Arrays.toString(coeff));
