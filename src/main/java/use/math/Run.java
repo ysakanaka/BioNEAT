90677167100a74716623864ddea97b713978aa10
unknown <huystalker@gmail.com>
2015-07-07 14:26:42 +0900

    Use default parameter set

--- src/main/java/use/math/Run.java
+++ src/main/java/use/math/Run.java
@@ -56,8 +56,8 @@ public class Run {
 
 		Population population = new Population(500, Library.startingMath);
 		population.setFitnessFunction(new GaussianFitnessFunction());
-		population.setMutator(new Mutator(new ArrayList<MutationRule>(Arrays.asList(new MutationRule[] { new DisableTemplate(3),
-				new MutateParameter(970), new AddNode(6), new AddActivation(6), new AddInhibition(15) }))));
+		population.setMutator(new Mutator(new ArrayList<MutationRule>(Arrays.asList(new MutationRule[] { new DisableTemplate(1),
+				new MutateParameter(90), new AddNode(2), new AddActivation(2), new AddInhibition(5) }))));
 		for (int i = 0; i < 10000; i++) {
 			if (i == 0) {
 				population.resetPopulation();
