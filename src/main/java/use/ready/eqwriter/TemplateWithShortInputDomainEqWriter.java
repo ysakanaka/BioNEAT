b4b1a48a5bdf18f26d28dd3c166031e2563c74d0
Nauberkato <nauberkato@dhcp48-112.admp6.jussieu.fr>
2016-06-21 14:47:42 +0200

    PseudoTemplateEqWriter

--- src/main/java/use/ready/eqwriter/TemplateWithShortInputDomainEqWriter.java
+++ src/main/java/use/ready/eqwriter/TemplateWithShortInputDomainEqWriter.java
@@ -5,14 +5,17 @@ import model.chemicals.SequenceVertex;
 
 public class TemplateWithShortInputDomainEqWriter<E> extends DefaultTemplateEqWriter<E> {
 	
+	String nameinBase;
+	String missingBaseSlowdown = "mbSlowdown"; //how slow is it to attach
+	
 	public TemplateWithShortInputDomainEqWriter(OligoGraph<SequenceVertex, E> g, E t) {
-		super(g, t);
-		
+		this(g, t,true);
 	}
 
 	public TemplateWithShortInputDomainEqWriter(OligoGraph<SequenceVertex, E> g, E t, boolean numerical) {
 		super(g, t, numerical);
-		
+		nameinBase = namein; //in case the trick is not valid
+		namein += " * "+missingBaseSlowdown+t.toString().replace("->", "to"); //trick, valid since namein is only used for hybridization.
 	}
 
 }
