9002cb9030042f671680be74880e4f06e83efe3c
Nathanael Aubert-Kato <nathanael.aubert@gmail.com>
2016-07-28 10:49:20 +0900

    With step function (bug?)

---
+++ src/main/java/use/math/step/StepFitnessDisplayer.java
@@ -0,0 +1,46 @@
+package use.math.step;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JPanel;
+
+import org.apache.commons.math3.special.Erf;
+
+import erne.AbstractFitnessResult;
+import erne.FitnessDisplayer;
+import model.PlotFactory;
+import use.math.FitnessResult;
+
+public class StepFitnessDisplayer implements FitnessDisplayer {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public JPanel drawVisualization(AbstractFitnessResult fitness) {
+		FitnessResult fitnessResult = (FitnessResult) fitness;
+		if (!fitnessResult.minFitness) {
+			double[] targetOutputs = new double[fitnessResult.inputs.length];
+			for (int k = 0; k < targetOutputs.length; k++) {
+				int offset = (StepFitnessFunction.hysteresis && k >= targetOutputs.length/2)?StepFitnessFunction.hystCoeff.length/2:0;
+				double[] coeffs = (StepFitnessFunction.hysteresis?(StepFitnessFunction.diff?StepFitnessFunction.hystdiffCoeff:StepFitnessFunction.hystCoeff):StepFitnessFunction.targetCoeff);
+				targetOutputs[k] = coeffs[0+offset]*(0.5+0.5*Erf.erf((fitnessResult.inputs[k]-coeffs[1+offset])/coeffs[3+offset]))+coeffs[2+offset];
+			}
+			Map<String, double[]> timeSeries = new HashMap<String, double[]>();
+			timeSeries.put("Actual outputs", fitnessResult.actualOutputs);
+			timeSeries.put("Fitted outputs", fitnessResult.targetOutputs);
+			timeSeries.put("Target outputs", targetOutputs);
+			double[] xData = new double[fitnessResult.inputs.length];
+			for (int k = 0; k < xData.length; k++) {
+				xData[k] = fitnessResult.inputs[k];
+			}
+			return new PlotFactory().createTimeSeriesPanel(timeSeries, xData, false, "Input concentration", "Output concentration");
+		} else {
+			return new JPanel();
+		}
+	}
+
+}
