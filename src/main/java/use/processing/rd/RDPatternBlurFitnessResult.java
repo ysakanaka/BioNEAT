f700b16b6ca1487500ad24d5ed04b64993247588
Naubertkato <nathanael.aubert@gmail.com>
2016-12-16 16:22:33 +0100

    again

--- src/main/java/use/processing/rd/RDPatternBlurFitnessResult.java
+++ src/main/java/use/processing/rd/RDPatternBlurFitnessResult.java
@@ -32,7 +32,7 @@ public class RDPatternBlurFitnessResult extends RDPatternFitnessResult {
 		for(int i=0; i<dists.length; i++){
 			for(int j=0; j<dists[i].length; j++){
 				float localconc = conc[RDConstants.glueIndex][i][j];
-				res += dists[i][j]*localconc;
+				if(localconc > RDConstants.cutOff) res += dists[i][j]*localconc;
 				totalConc += localconc;
 			}
 		}
